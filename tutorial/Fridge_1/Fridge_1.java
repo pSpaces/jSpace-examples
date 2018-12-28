d8131367fe8d3358036ce9158dd2b6e0f860cc8c
Alberto Lluch Lafuente <albertolluch@gmail.com>
2018-12-28 17:00:38 +0100

    Create Fridge_1.java

---
+++ tutorial/Fridge_1/Fridge_1.java
@@ -0,0 +1,106 @@
+package common.src.main;
+
+import org.jspace.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Fridge_1 {
+
+	public static void main(String[] argv) throws InterruptedException {
+
+		// Creating a space.
+		Space fridge = new SequentialSpace(); 
+
+		new Thread(new Alice(fridge)).start();
+		new Thread(new Bob(fridge)).start();
+		new Thread(new Charlie(fridge)).start();
+
+	}
+
+}
+
+class Alice implements Runnable {
+	private Space fridge;
+
+	public Alice(Space fridge){
+		this.fridge = fridge;
+	}
+
+	@Override
+	public void run() {
+		try {
+			fridge.put("milk", 2);
+			fridge.put("butter", 1);
+			fridge.put("shop!");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+
+class Bob implements Runnable {
+	private Space fridge;
+
+	public Bob(Space fridge){
+		this.fridge = fridge;
+	}
+
+
+	@Override
+	public void run() {
+		try{
+			fridge.query(new ActualField("shop!"));
+			while (true) {
+				Object[] item = fridge.get(new FormalField(String.class), new FormalField(Integer.class));
+				System.out.println("Bob: I am shopping " + item[1] +" items of " + item[0] + "...");
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
+
+class Charlie implements Runnable {
+	private Space fridge;
+
+	public Charlie(Space fridge){
+		this.fridge = fridge;
+	}
+
+
+	@Override
+	public void run() {
+		try{
+			//var item string
+			//var quantity int
+			//_, err := fridge.GetP("shop!")
+			//if err == nil {
+			//	for {
+			//		t, _ := fridge.Get(&item, &quantity)
+			//		item = (t.GetFieldAt(0)).(string)
+			//		quantity = (t.GetFieldAt(1)).(int)
+			//		fmt.Printf("Charlie: I am shopping %d items of %s...\n", quantity, item)
+			//	}
+			//} else {
+			//	fmt.Printf("Charlie: I am just relaxing...\n")
+			//}
+			Object[] t = fridge.queryp(new ActualField("shop!"));
+			if (t != null) {
+				while (true) {
+					Object[] item = fridge.get(new FormalField(String.class), new FormalField(Integer.class));
+					System.out.println("Charlie: I am shopping " + item[1] +" items of " + item[0] + "...");
+				}
+			} else {
+				System.out.println("Charlie: I am just relaxing...");
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
